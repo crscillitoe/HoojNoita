516b94866cc984e61e41d8471e67de5ca73ce99e
crscillitoe <christian.scillitoe@gmail.com>
2024-06-07 19:40:01 -0700

    Remove example mod boilerplatet

--- init.lua
+++ init.lua
@@ -1,15 +1,11 @@
 dofile_once( "data/scripts/lib/utilities.lua" )
 
-
-
 local pollnet = require('mods\\hoojMod\\pollnet')
 local json = require('mods\\hoojMod\\json')
 
 
 -- Reactor is a convenience for running Lua coroutines
 local reactor = pollnet.Reactor()
-
--- all functions below are optional and can be left out
 
 function OnModPreInit()
 	print("Hooj - OnModPreInit()") -- First this is called for all mods
@@ -22,6 +18,11 @@ function OnModInit()
 	-- Connect to hooj eventstream
 	-- https://overlay.woohooj.in/stream/?channel=events
 	reactor:run(function()
+
+		-- Dark wizard arts.
+		-- Trick  pollnet into thinking this is a tcp socket, but then just manually send
+		-- an HTTP GET and listen to the eventstream instead
+		-- IMPORTANT, HTTP messages must end with two newlines.
 		local req_sock = pollnet.open_tcp("69.55.54.58:80")
 		req_sock:send("GET /stream/?channel=simple-chat HTTP/1.1\r\nHost: overlay.woohooj.in\r\nUser-Agent: curl/7.85.0\r\nAccept: */*\r\n\r\n")
 
@@ -59,20 +60,27 @@ function OnModInit()
 			local content = decoded.content
 			local author = decoded.author_id
 
+			-- TODO: Parse message for 1/2/3/4 and send to vote
 			print(content)
 
 			::continue::
 		end
 	end)
-
-	-- Print chat messages
 end
+
+
+function OnWorldPostUpdate() -- This is called every time the game has finished updating the world
+	-- Reactor.update() enables us to
+	-- essentially run C coroutines for our eventstream
+	reactor:update()
+end
+
+--[[
 
 function OnModPostInit()
 	StreamingSetVotingEnabled(true)
 end
 
---[[
 function OnPlayerSpawned( player_entity ) -- This runs when player entity has been created
 	GamePrint( "OnPlayerSpawned() - Player entity id: " .. tostring(player_entity) )
 end
@@ -85,69 +93,8 @@ function OnWorldPreUpdate() -- This is called every time the game is about to st
 	GamePrint( "Pre-update hook " .. tostring(GameGetFrameNum()) )
 end
 
-]]--
-
-function OnWorldPostUpdate() -- This is called every time the game has finished updating the world
-	GamePrint( "Post-update hook " .. tostring(GameGetFrameNum()) )
-	reactor:update()
-end
-
-
 function OnMagicNumbersAndWorldSeedInitialized() -- this is the last point where the Mod* API is available. after this materials.xml will be loaded.
 	local x = ProceduralRandom(0,0)
 	print( "===================================== random " .. tostring(x) )
 end
-
-
--- This code runs when all mods' filesystems are registered
-ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/example/files/actions.lua" ) -- Basically dofile("mods/example/files/actions.lua") will appear at the end of gun_actions.lua
-ModMagicNumbersFileAdd( "mods/example/files/magic_numbers.xml" ) -- Will override some magic numbers using the specified file
-
--- see tools_modding/noita_fmod_project/ for the project that defines the audio events used in this mod
-ModRegisterAudioEventMappings( "mods/example/files/audio_events.txt" ) -- Use this to register custom fmod events. Event mapping files can be generated via File -> Export GUIDs in FMOD Studio.
-ModRegisterMusicBank( "mods/example/files/music.bank" ) -- Use this to register custom banks in the music system. ModRegisterAudioEventMappings also needs to be called to make the game recognize the events.
-
-ModMaterialsFileAdd( "mods/example/files/materials_rainbow.xml" ) -- Adds a new 'rainbow' material to materials
-ModLuaFileAppend( "data/scripts/items/potion.lua", "mods/example/files/potion_appends.lua" )
-
--- make coalmine play music from the new bank
-local coal_xml = ModTextFileGetContent( "data/biome/coalmine.xml" )
-coal_xml = coal_xml:gsub([[audio_music_2="coalmine"]], [[audio_music_2="thebiome"]] )
-ModTextFileSetContent( "data/biome/coalmine.xml", coal_xml )
-
-
-if ModImageMakeEditable ~= nil then -- needed to avoid error if this file is hotloaded after init
-	-- make the player sprite green
-	local t1 = GameGetRealWorldTimeSinceStarted();
-
-	local recolor = function( filename)
-		local id,w,h = ModImageMakeEditable( filename, 0, 0 )
-		for y=0,h do
-			for x=0,w do
-				local c = ModImageGetPixel( id, x, y )
-				local r,g,b,a = color_abgr_split( c )
-				r = r / 2
-				b = b / 2
-				c = color_abgr_merge(r,g,b,a)
-				ModImageSetPixel( id, x, y, c )
-			end
-		end
-	end
-
-	-- using different slots here to test the feature works,
-	-- but you could use the same slot when editing images independent of each other like this
-	recolor( "data/enemies_gfx/player.png" )
-	recolor( "data/enemies_gfx/player_arm.png" )
-	recolor( "data/enemies_gfx/player_arm_no_item.png" )
-
-	t1 = GameGetRealWorldTimeSinceStarted() -t1
-	print("ModImage stuff took " .. t1 .. " seconds")
-
-	local who_edited = ModImageWhoSetContent( "data/enemies_gfx/player.png" )
-	local ok = " - fail"
-	if who_edited == "example" then ok = " - ok" end
-
-	print( "Who edited? " .. who_edited .. ok )
-end
-
---print("Example mod init done")
+]]--
